5efdeacb9a7b0d3e410280f64a3845a4a8e38ca6
Gajo Petrovic <gajo01@gmail.com>
2015-11-20 17:15:15 +0900

    remove redundant config lines

--- luaui/config/globals.lua
+++ luaui/config/globals.lua
@@ -25,15 +25,3 @@ config = {
 		subsuggestionColor = { 0, 0, 0, 0 },
 	},
 }
-
--- Config
-local consoleX, consoleY = 0.26, 0.25
-local consoleWidth = 0.5
-local suggestionsHeight = 0.4
-local suggesitonFontSize = 16
-local suggesitonPadding = 4
-local pageUpFactor = 10
-local pageDownFactor = 10
-local selectedSuggestionColor = { 0, 1, 1, 0.4 }
-local subsuggestionColor = { 0, 0, 0, 0 }
-local fontFile = "LuaUI/fonts/dejavu-sans-mono/DejaVuSansMono.ttf"
